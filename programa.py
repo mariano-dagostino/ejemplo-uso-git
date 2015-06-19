169dc247509cc2bd2681c51471b0ae513edd6130
Mariano D'Agostino <dagmar@154086.no-reply.drupal.org>
2015-06-18 22:49:17 -0300

    Le permito al usuario que ingrese los datos

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
-numero = 20
-otro_numero = 30
+numero = raw_input("Ingrese un numero:")
+otro_numero = raw_input("Ingrese otro numero:")
 
-resultado = numero + otro_numero
-print resultado
+resultado = int(numero) + int(otro_numero)
+print "El resultado es " + str(resultado)
